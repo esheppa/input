4c87c55c8cb45e65415ab62fedb8d59c58a3e632
Eric Sheppard <eric@sheppard.cloud>
2021-06-05 15:09:15 +1000

    init

---
+++ src/inputs.rs
@@ -0,0 +1,653 @@
+use chrono::Datelike;
+use resolution::DateResolution as DateResolutionTrait;
+use std::{cmp, collections, convert, error, fmt, marker, num};
+
+#[derive(Debug)]
+pub struct SelectError {
+    selected: String,
+}
+
+impl SelectError {
+    pub fn new(selected: String) -> SelectError {
+        SelectError { selected }
+    }
+}
+
+impl fmt::Display for SelectError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Value {} is not in the list of allowed options",
+            self.selected
+        )
+    }
+}
+
+impl error::Error for SelectError {}
+
+pub type BasicSelect = Select<convert::Infallible, String>;
+
+pub struct Select<E: error::Error + Sync + Send + 'static, O: fmt::Display + Ord + std::str::FromStr<Err = E>> {
+    input: String,
+    options: collections::BTreeSet<O>,
+    o: marker::PhantomData<O>,
+}
+
+impl<E: error::Error + Sync + Send + 'static, O: fmt::Display + Ord + std::str::FromStr<Err = E>> Select<E, O> {
+    pub fn get_input(&self) -> &str {
+        &self.input
+    }
+    pub fn new(data: O, options: collections::BTreeSet<O>) -> Select<E, O> {
+        Select {
+            input: data.to_string(),
+            options,
+            o: marker::PhantomData,
+        }
+    }
+}
+
+impl<E: error::Error + Sync + Send + 'static, O: fmt::Display + Ord + std::str::FromStr<Err = E>> crate::UserInput
+    for Select<E, O>
+{
+    type Output = O;
+    type Input = String;
+
+    fn update(&mut self, input: Self::Input) {
+        self.input = input;
+    }
+    fn parse(&self) -> crate::Result<Self::Output> {
+        let parsed = self.input.parse()?;
+        if self.options.contains(&parsed) {
+            Ok(parsed)
+        } else {
+            Err(SelectError {
+                selected: self.input.to_string(),
+            }
+            .into())
+        }
+    }
+}
+
+pub struct RelationalSelect<
+    E: error::Error + Sync + Send + 'static,
+    K: std::str::FromStr<Err = E> + cmp::Ord,
+    V,
+> {
+    input: String,
+    options: collections::BTreeMap<K, V>,
+    k: marker::PhantomData<K>,
+}
+
+impl<E: error::Error + Sync + Send + 'static, K: std::str::FromStr<Err = E> + cmp::Ord, V>
+    RelationalSelect<E, K, V>
+{
+    pub fn get_input(&self) -> &str {
+        &self.input
+    }
+    pub fn new(input: String, options: collections::BTreeMap<K, V>) -> RelationalSelect<E, K, V> {
+        RelationalSelect {
+            input,
+            options,
+            k: marker::PhantomData,
+        }
+    }
+}
+
+impl<E: error::Error + Sync + Send + 'static, K: std::str::FromStr<Err = E> + cmp::Ord, V>
+    crate::UserInput for RelationalSelect<E, K, V>
+{
+    type Output = K;
+    type Input = String;
+
+    fn update(&mut self, input: Self::Input) {
+        self.input = input;
+    }
+    fn parse(&self) -> crate::Result<Self::Output> {
+        let key = self.input.parse()?;
+
+        if self.options.contains_key(&key) {
+            Ok(key)
+        } else {
+            Err(SelectError {
+                selected: self.input.to_string(),
+            }
+            .into())
+        }
+    }
+}
+
+pub struct Scalar<O, E>
+where
+    O: std::str::FromStr<Err = E> + fmt::Display,
+    E: error::Error + Sync + Send + 'static,
+{
+    input: String,
+    validations: crate::Validations<O>,
+    o: marker::PhantomData<O>,
+}
+
+impl<O, E> Scalar<O, E>
+where
+    O: std::str::FromStr<Err = E> + fmt::Display,
+    E: error::Error + Sync + Send + 'static,
+{
+    pub fn get_input(&self) -> &str {
+        &self.input
+    }
+    pub fn new(data: &O, validations: crate::Validations<O>) -> Scalar<O, E> {
+        Scalar {
+            input: data.to_string(),
+            validations,
+            o: marker::PhantomData,
+        }
+    }
+}
+
+impl<O, E> Default for Scalar<O, E>
+where
+    O: std::str::FromStr<Err = E> + Default + fmt::Display,
+    E: error::Error + Sync + Send + 'static,
+{
+    fn default() -> Scalar<O, E> {
+        Scalar::new(&O::default(), crate::Validations::new())
+    }
+}
+
+impl<O, E> crate::UserInput for Scalar<O, E>
+where
+    O: std::str::FromStr<Err = E> + fmt::Display,
+    E: error::Error + Sync + Send + 'static,
+{
+    type Output = O;
+    type Input = String;
+    fn update(&mut self, input: Self::Input) {
+        self.input = input;
+    }
+    fn parse(&self) -> crate::Result<Self::Output> {
+        let parsed = self.input.parse()?;
+        self.validations.validate(&parsed)?;
+        Ok(parsed)
+    }
+}
+
+pub type Integer<I> = Scalar<I, num::ParseIntError>;
+pub type Decimal = Scalar<rust_decimal::Decimal, rust_decimal::Error>;
+pub type Text = Scalar<String, convert::Infallible>;
+
+pub struct NaiveDate {
+    input: String,
+    format: &'static str,
+    validations: crate::Validations<chrono::NaiveDate>,
+}
+
+impl NaiveDate {
+    pub fn get_input(&self) -> &str {
+        &self.input
+    }
+    pub fn new(
+        data: chrono::NaiveDate,
+        format: &'static str,
+        validations: crate::Validations<chrono::NaiveDate>,
+    ) -> NaiveDate {
+        NaiveDate {
+            input: data.format(format).to_string(),
+            format,
+            validations,
+        }
+    }
+}
+
+impl Default for NaiveDate {
+    fn default() -> NaiveDate {
+        NaiveDate::new(
+            chrono::Utc::now().date().naive_utc(),
+            "%Y-%m-%d",
+            crate::Validations::new(),
+        )
+    }
+}
+
+impl crate::UserInput for NaiveDate {
+    type Output = chrono::NaiveDate;
+    type Input = String;
+    fn update(&mut self, input: Self::Input) {
+        self.input = input;
+    }
+    fn parse(&self) -> crate::Result<Self::Output> {
+        let parsed = chrono::NaiveDate::parse_from_str(&self.input, self.format)?;
+        self.validations.validate(&parsed)?;
+        Ok(parsed)
+    }
+}
+
+pub struct Date {
+    input: String,
+    format: &'static str,
+    validations: crate::Validations<resolution::Date>,
+}
+
+impl Default for Date {
+    fn default() -> Date {
+        Date::new(
+            chrono::Utc::now().date().naive_utc().into(),
+            "%Y-%m-%d",
+            crate::Validations::new(),
+        )
+    }
+}
+
+impl Date {
+    pub fn get_input(&self) -> &str {
+        &self.input
+    }
+    pub fn new(
+        data: resolution::Date,
+        format: &'static str,
+        validations: crate::Validations<resolution::Date>,
+    ) -> Date {
+        Date {
+            input: data.start().format(format).to_string(),
+            format,
+            validations,
+        }
+    }
+}
+
+impl crate::UserInput for Date {
+    type Output = resolution::Date;
+    type Input = String;
+    fn update(&mut self, input: Self::Input) {
+        self.input = input;
+    }
+    fn parse(&self) -> crate::Result<Self::Output> {
+        let parsed = chrono::NaiveDate::parse_from_str(&self.input, self.format)?.into();
+        self.validations.validate(&parsed)?;
+        Ok(parsed)
+    }
+}
+
+pub struct Year {
+    input: String,
+    validations: crate::Validations<resolution::Year>,
+}
+
+impl Default for Year {
+    fn default() -> Year {
+        Year::new(
+            resolution::Year::new(chrono::Utc::now().date().year()),
+            crate::Validations::new(),
+        )
+    }
+}
+
+impl Year {
+    pub fn get_input(&self) -> &str {
+        &self.input
+    }
+    pub fn new(data: resolution::Year, validations: crate::Validations<resolution::Year>) -> Year {
+        Year {
+            input: data.to_string(),
+            validations,
+        }
+    }
+}
+
+impl crate::UserInput for Year {
+    type Output = resolution::Year;
+    type Input = String;
+    fn update(&mut self, input: Self::Input) {
+        self.input = input;
+    }
+    fn parse(&self) -> crate::Result<Self::Output> {
+        let parsed = resolution::Year::new(self.input.parse()?);
+        self.validations.validate(&parsed)?;
+        Ok(parsed)
+    }
+}
+
+pub struct RelativeMonth {
+    input: String,
+    validations: crate::Validations<u32>,
+}
+
+impl Default for RelativeMonth {
+    fn default() -> RelativeMonth {
+        RelativeMonth::new(chrono::Utc::now().month(), crate::Validations::new())
+    }
+}
+
+impl RelativeMonth {
+    pub fn get_input(&self) -> &str {
+        &self.input
+    }
+    pub fn new(data: u32, validations: crate::Validations<u32>) -> RelativeMonth {
+        RelativeMonth {
+            input: data.to_string(),
+            validations,
+        }
+    }
+}
+
+impl crate::UserInput for RelativeMonth {
+    type Output = u32;
+    type Input = String;
+    fn update(&mut self, input: Self::Input) {
+        self.input = input;
+    }
+    fn parse(&self) -> crate::Result<Self::Output> {
+        let parsed = self.input.parse()?;
+        if parsed < 1 || parsed > 12 {
+            return Err(crate::Error::Validation(
+                vec![format!(
+                    "Month number should be between 1 and 12 but was {}",
+                    parsed
+                )]
+                .into(),
+            ));
+        };
+        self.validations.validate(&parsed)?;
+        Ok(parsed)
+    }
+}
+
+pub struct Month {
+    year: Year,
+    month: RelativeMonth,
+    validations: crate::Validations<resolution::Month>,
+}
+
+impl Default for Month {
+    fn default() -> Month {
+        Month {
+            validations: crate::Validations::new(),
+            ..Default::default()
+        }
+    }
+}
+
+pub enum MonthMsg {
+    Year(String),
+    Month(String),
+}
+
+impl Month {
+    pub fn get_year(&self) -> &Year {
+        &self.year
+    }
+    pub fn get_month(&self) -> &RelativeMonth {
+        &self.month
+    }
+    pub fn new(
+        input: resolution::Month,
+        year_validations: crate::Validations<resolution::Year>,
+        month_validations: crate::Validations<u32>,
+        validations: crate::Validations<resolution::Month>,
+    ) -> Month {
+        Month {
+            year: Year::new(input.year(), year_validations),
+            month: RelativeMonth::new(input.month_num(), month_validations),
+            validations,
+        }
+    }
+}
+
+impl crate::UserInput for Month {
+    type Output = resolution::Month;
+    type Input = MonthMsg;
+    fn update(&mut self, input: Self::Input) {
+        match input {
+            MonthMsg::Year(y) => self.year.input = y,
+            MonthMsg::Month(m) => self.month.input = m,
+        }
+    }
+    fn parse(&self) -> crate::Result<Self::Output> {
+        let year = self.year.parse()?;
+        let month = self.month.parse()?;
+        let parsed =
+            resolution::Month::from_date(chrono::NaiveDate::from_ymd(year.year_num(), month, 1));
+        self.validations.validate(&parsed)?;
+        Ok(parsed)
+    }
+}
+
+pub struct RelativeQuarter {
+    input: String,
+    validations: crate::Validations<u32>,
+}
+
+impl RelativeQuarter {
+    pub fn get_input(&self) -> &str {
+        &self.input
+    }
+    pub fn new(data: u32, validations: crate::Validations<u32>) -> RelativeQuarter {
+        // we can accept data other than 1..4 as it will just result in a failed .parse()
+        RelativeQuarter {
+            input: data.to_string(),
+            validations,
+        }
+    }
+}
+
+impl crate::UserInput for RelativeQuarter {
+    type Output = u32;
+    type Input = String;
+    fn update(&mut self, input: Self::Input) {
+        self.input = input;
+    }
+    fn parse(&self) -> crate::Result<Self::Output> {
+        let parsed = self.input.parse()?;
+        if parsed < 1 || parsed > 4 {
+            return Err(crate::Error::Validation(
+                vec![format!(
+                    "Quarter number should be between 1 and 4 but was {}",
+                    parsed
+                )]
+                .into(),
+            ));
+        };
+        self.validations.validate(&parsed)?;
+        Ok(parsed)
+    }
+}
+
+impl Default for RelativeQuarter {
+    fn default() -> RelativeQuarter {
+        RelativeQuarter::new(
+            (chrono::Utc::now().month() + 2) / 3,
+            crate::Validations::new(),
+        )
+    }
+}
+
+pub struct Quarter {
+    year: Year,
+    quarter: RelativeQuarter,
+    validations: crate::Validations<resolution::Quarter>,
+}
+
+impl Default for Quarter {
+    fn default() -> Quarter {
+        Quarter {
+            validations: crate::Validations::new(),
+            ..Default::default()
+        }
+    }
+}
+
+pub enum QuarterMsg {
+    Year(String),
+    Quarter(String),
+}
+
+impl Quarter {
+    pub fn get_year(&self) -> &Year {
+        &self.year
+    }
+    pub fn get_quarter(&self) -> &RelativeQuarter {
+        &self.quarter
+    }
+    pub fn new(
+        input: resolution::Quarter,
+        year_validations: crate::Validations<resolution::Year>,
+        quarter_validations: crate::Validations<u32>,
+        validations: crate::Validations<resolution::Quarter>,
+    ) -> Quarter {
+        Quarter {
+            year: Year::new(input.year(), year_validations),
+            quarter: RelativeQuarter::new(input.quarter_num(), quarter_validations),
+            validations,
+        }
+    }
+}
+
+impl crate::UserInput for Quarter {
+    type Output = resolution::Quarter;
+    type Input = QuarterMsg;
+    fn update(&mut self, input: Self::Input) {
+        match input {
+            QuarterMsg::Year(y) => self.year.input = y,
+            QuarterMsg::Quarter(m) => self.quarter.input = m,
+        }
+    }
+    fn parse(&self) -> crate::Result<Self::Output> {
+        let year = self.year.parse()?;
+        let quarter = self.quarter.parse()?;
+        let parsed = resolution::Quarter::from_date(chrono::NaiveDate::from_ymd(
+            year.year_num(),
+            quarter * 3 - 2,
+            1,
+        ));
+        self.validations.validate(&parsed)?;
+        Ok(parsed)
+    }
+}
+
+pub struct DateResolution<I, R>
+where
+    R: resolution::DateResolution,
+    I: crate::UserInput<Output = R> + Default,
+{
+    input: I,
+    _r: marker::PhantomData<R>,
+}
+
+impl<I, R> DateResolution<I, R>
+where
+    R: resolution::DateResolution,
+    I: crate::UserInput<Output = R> + Default,
+{
+    pub fn new(input: I) -> DateResolution<I, R> {
+        DateResolution {
+            _r: marker::PhantomData,
+            input,
+        }
+    }
+}
+
+impl<I, R> From<I> for DateResolution<I, R>
+where
+    R: resolution::DateResolution,
+    I: crate::UserInput<Output = R> + Default,
+{
+    fn from(input: I) -> DateResolution<I, R> {
+        DateResolution::new(input)
+    }
+}
+
+impl<I, R> Default for DateResolution<I, R>
+where
+    R: resolution::DateResolution,
+    I: crate::UserInput<Output = R> + Default,
+{
+    fn default() -> DateResolution<I, R> {
+        DateResolution::new(I::default())
+    }
+}
+
+impl<I, R> crate::UserInput for DateResolution<I, R>
+where
+    R: resolution::DateResolution,
+    I: crate::UserInput<Output = R> + Default,
+{
+    type Output = I::Output;
+    type Input = I::Input;
+    fn update(&mut self, input: Self::Input) {
+        self.input.update(input);
+    }
+    fn parse(&self) -> crate::Result<Self::Output> {
+        Ok(self.input.parse()?)
+    }
+}
+
+pub struct TimeRange<I, R>
+where
+    R: resolution::DateResolution,
+    I: crate::UserInput<Output = R> + Default,
+{
+    date_resolution: DateResolution<I, R>,
+    length: Integer<u32>,
+    length_validations: crate::Validations<u32>,
+    range_validations: crate::Validations<resolution::TimeRange<R>>,
+    _r: marker::PhantomData<R>,
+}
+
+fn greater_than_zero(input: &u32) -> crate::ValidationResult {
+    if input > &0 {
+        Ok(())
+    } else {
+        Err("Input must be greater than zero".to_string())
+    }
+}
+
+impl<I, R> TimeRange<I, R>
+where
+    R: resolution::DateResolution,
+    I: crate::UserInput<Output = R> + Default,
+{
+    pub fn new(
+        data: resolution::TimeRange<R>,
+        dr_input: I,
+        length_validations: crate::Validations<u32>,
+        range_validations: crate::Validations<resolution::TimeRange<R>>,
+    ) -> TimeRange<I, R> {
+        TimeRange {
+            _r: marker::PhantomData,
+            date_resolution: DateResolution::new(dr_input),
+            length: Integer::new(&1, crate::Validations::from_vec(vec![greater_than_zero])),
+            length_validations,
+            range_validations,
+        }
+    }
+}
+
+pub enum TimeRangeMsg<I, R>
+where
+    R: resolution::DateResolution,
+    I: crate::UserInput<Output = R> + Default,
+{
+    DateResolution(I::Input),
+    Length(String),
+}
+
+impl<I, R> crate::UserInput for TimeRange<I, R>
+where
+    R: resolution::DateResolution,
+    I: crate::UserInput<Output = R> + Default,
+{
+    type Output = resolution::TimeRange<R>;
+    type Input = TimeRangeMsg<I, R>;
+    fn update(&mut self, input: Self::Input) {
+        match input {
+            TimeRangeMsg::DateResolution(input) => self.date_resolution.update(input),
+            TimeRangeMsg::Length(input) => self.length.update(input),
+        }
+    }
+    fn parse(&self) -> crate::Result<Self::Output> {
+        let start = self.date_resolution.parse()?;
+        let len = self.length.parse()?;
+        self.length_validations.validate(&len)?;
+        let range = resolution::TimeRange::new(start, len);
+        self.range_validations.validate(&range)?;
+        Ok(range)
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,190 @@
+use std::{collections, error, fmt, result};
+
+pub mod inputs;
+
+// it doesn't make sense to ever have both kinds of errors
+// as it should be impossible for us to validate if we couldn't
+// first succesfully parse the value
+// this purposely doesn't impl std::error::Error to allow the from
+// impls below.
+//
+// This makes sense as the primary use of this error is either:
+// - the proc macro that goes from ui backing to regular struct
+//      * this will probably output a regular Error impl
+// - a display funciton
+// both of which would consume the error rather than bubbling
+// it up with a `?`
+#[derive(Debug)]
+#[non_exhaustive]
+pub enum Error {
+    Parse(Box<dyn error::Error + Sync + Send + 'static>),
+    Validation(ValidationErrors),
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::Parse(e) => write!(f, "parse error {}", e),
+            Error::Validation(e) => write!(f, "validation error(s) {}", e),
+        }
+    }
+}
+
+type ValidationFn<T> = fn(&T) -> ValidationResult;
+
+pub struct Validations<T> {
+    funcs: Vec<ValidationFn<T>>,
+}
+
+impl<T> Validations<T> {
+    pub fn new() -> Validations<T> {
+        Validations { funcs: Vec::new() }
+    }
+    pub fn from_vec(funcs: Vec<ValidationFn<T>>) -> Self {
+        Validations { funcs }
+    }
+    fn validate(&self, input: &T) -> result::Result<(), ValidationErrors> {
+        let errors = self
+            .funcs
+            .iter()
+            .map(|f| f(input))
+            .filter_map(|r| if let Err(e) = r { Some(e) } else { None })
+            .collect::<Vec<_>>();
+        if errors.is_empty() {
+            Ok(())
+        } else {
+            Err(ValidationErrors { errors })
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct ValidationErrors {
+    errors: Vec<String>,
+}
+
+impl std::fmt::Display for ValidationErrors {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let formatted =
+            itertools::Itertools::intersperse(self.errors.iter().cloned(), ", ".to_string())
+                .collect::<String>();
+        write!(f, "[{}]", formatted)
+    }
+}
+
+pub type ValidationResult = result::Result<(), String>;
+
+pub type FormResult<T> = result::Result<T, FormError>;
+
+#[derive(Debug)]
+pub struct FormError {
+    errors: collections::BTreeMap<&'static str, Error>,
+}
+
+impl std::fmt::Display for FormError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let iter = self
+            .errors
+            .iter()
+            .map(|(field, issue)| format!("{}: {}", field, issue));
+        write!(
+            f,
+            "{}",
+            itertools::Itertools::intersperse(iter, ", ".to_string()).collect::<String>()
+        )
+    }
+}
+
+impl std::error::Error for FormError {}
+
+impl FormError {
+    pub fn is_empty(&self) -> bool {
+        self.errors.is_empty()
+    }
+    pub fn new() -> FormError {
+        FormError {
+            errors: collections::BTreeMap::new(),
+        }
+    }
+    pub fn add_result<T>(&mut self, field: &'static str, result: Result<T>) {
+        if let Some(err) = result.err() {
+            self.errors.insert(field, err);
+        }
+    }
+    pub fn add_error(&mut self, field: &'static str, err: Error) {
+        self.errors.insert(field, err);
+    }
+}
+
+impl From<ValidationErrors> for Error {
+    fn from(validation: ValidationErrors) -> Error {
+        Error::Validation(validation)
+    }
+}
+impl From<Vec<String>> for ValidationErrors {
+    fn from(errors: Vec<String>) -> ValidationErrors {
+        ValidationErrors { errors }
+    }
+}
+
+impl<T> From<T> for Error
+where
+    T: error::Error + Sync + Send + 'static,
+{
+    fn from(t: T) -> Error {
+        Error::Parse(Box::new(t))
+    }
+}
+
+//  we force a custom result type here
+//  as otherwise not much useful work is done within parse
+//  and this also enables more useful use of the impls
+pub type Result<T> = result::Result<T, Error>;
+
+// principals of input backing:
+// 1. View parts must be created manually by the user (to allow layout etc - implicit here is that it is up to the user to ensure 
+//    all inputs are created)
+// 2. Prebuilt functions that create the inputs should be used to create the view
+// 3. It should be difficult for the user to create inputs that message non-existant fields.
+// 4. Have to allow for the fact that a single property may be inputted via multiple inputs (eg MonthYear created by a Month and
+//    Year inputs)
+// 5. Validatiors from the main struct should be automatically applied to the relevant fields of the input
+// 6. Validators shoudl be able to be applied to each field whether or not all are valid.
+// 7. Allow deriving ToInputMap and FromInputMap
+// 8. Each datatype in the main struct should implement ui::Input trait to allow for ToInputMap to work
+// 9. Message type should always be the same and then get mapped at a higher layer
+// 10. ui::Input implementations should handle whether the input creates a single or multiple actual inputs and handle 
+//     the relevant messages
+
+
+// user impls this on their local type, say 'DecimalInput'
+// then the user impls the ui rendering as inherent fns on that type
+// this is because that type may need to be rendered as multiple inputs
+// user also defines a message type.
+// The UserInput trait then ties together that Type with the Message type
+// and provides a standard interface for updating, parsing and getting the input data
+//
+// While it would be nice to either require that `Output: Into<Self>` or have a `new(data:
+// Self::Output) -> Self method here, this is too inflexible as the whole point of allowing the
+// user to specify their own UserInput types is to allow them to have their own extra fields
+// and behaviour that they want. This has the disadvantage of not allowing generation of the
+// Self::Output -> Output function, but has the benefit of allowing the user to specify eg
+// the options of a select at runtime (this is a fundamental requirement for `RelationalSelect`!)
+//
+/// By convention, users should create a `new` style function that takes a Self::Output, some other
+/// stuff, and produces a Self.
+pub trait UserInput {
+    type Output;
+    type Input;
+    fn update(&mut self, input: Self::Input);
+    fn parse(&self) -> Result<Self::Output>;
+}
+
+/// By convention, users should create a `new` style function that takes a Self::Output, some other
+/// stuff, and produces a Self.
+pub trait Form: Sized {
+    type Msg;
+    type Output;
+    fn update(&mut self, input: Self::Msg);
+    fn parse(&self) -> result::Result<Self::Output, FormError>;
+}
